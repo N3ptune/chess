3ac6c6e7785e41bd04f63895ccfeaf77c9e792b2
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-14 15:46:29 -0600

    flush out interfaces

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,4 +1,19 @@
 package dataaccess;
 
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
 public interface DataAccess {
+    void clear();
+    void createUser();
+    UserData getUser();
+    void createAuth();
+    AuthData getAuth();
+    int createGame(GameData game);
+    GameData getGame(int gameID);
+    Collection<GameData> listGames();
+    void updateGame(GameData updated);
 }
