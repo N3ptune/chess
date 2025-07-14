0837420c3cedcc9fff2fb4ee4f79cd2d2edd3b2f
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-14 15:24:17 -0600

    Add the modules but it's still not working

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,13 @@
 package service;
 
+import model.request.LoginRequest;
+import model.request.LogoutRequest;
+import model.request.RegisterRequest;
+import model.result.LoginResult;
+import model.result.RegisterResult;
+
 public class UserService {
+    public RegisterResult register(RegisterRequest registerRequest) {return new RegisterResult();}
+    public LoginResult login(LoginRequest loginRequest) {return new LoginResult();}
+    public void logout(LogoutRequest logoutRequest) {}
 }
