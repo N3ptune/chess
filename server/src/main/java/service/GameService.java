f61dc5268b32cbbb5c65e274e5d7f870688f8c9c
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-15 11:55:38 -0600

    implement joingame for service, three to go

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,17 +1,54 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDAO;
+import model.AuthData;
 import model.GameData;
 import model.request.JoinGameRequest;
 import model.request.CreateGameRequest;
 import model.result.CreateGameResult;
 import model.result.JoinGameResult;
 import model.result.ListGamesResult;
+import model.result.LogoutResult;
 
 
 // This will have Join Game, Create Game, and List Games
 
 public class GameService {
-    public JoinGameResult joinGame(JoinGameRequest joinGame){return new JoinGameResult();}
+
+    private final DataAccess dao = new MemoryDAO();
+
+    public JoinGameResult joinGame(JoinGameRequest joinGame){
+        try {
+            AuthData authData = dao.getAuth(joinGame.authToken());
+            GameData game = dao.getGame(joinGame.gameID());
+
+            if (!joinGame.authToken().equals(authData.authToken())){
+                return new JoinGameResult("Error: unauthorized");
+            }
+
+            if (joinGame.playerColor() == ChessGame.TeamColor.WHITE){
+                if (game.whiteUsername() != null){
+                    return new JoinGameResult("Error: color already taken");
+                }
+            } else if (joinGame.playerColor() == ChessGame.TeamColor.BLACK){
+                if (game.blackUsername() != null) {
+                    return new JoinGameResult("Error: color already taken");
+                }
+            } else {
+                return new JoinGameResult("Error: not a valid color");
+            }
+
+            dao.joinGame(game.gameID(), authData.username(), joinGame.playerColor());
+
+            return new JoinGameResult(null);
+
+        } catch (DataAccessException e){
+            return new JoinGameResult("Error: " + e.getMessage());
+        }
+    }
     public CreateGameResult createGame(CreateGameRequest createGame){return new CreateGameResult();}
     public ListGamesResult listGames(String authToken){
         return new ListGamesResult();
