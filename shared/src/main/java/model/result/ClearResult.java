4ce1fa78f2ea72c6cd7998178678a90ea9af5b91
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-15 11:21:48 -0600

    Make a message field for each result so I can return errors

--- shared/src/main/java/model/result/ClearResult.java
+++ shared/src/main/java/model/result/ClearResult.java
@@ -1,4 +1,4 @@
 package model.result;
 
-public record ClearResult() {
+public record ClearResult(String message) {
 }
--- shared/src/main/java/model/result/ListGamesResult.java
+++ shared/src/main/java/model/result/ListGamesResult.java
@@ -1,4 +1,8 @@
 package model.result;
 
-public record ListGamesResult() {
+import model.GameData;
+
+import java.util.Collection;
+
+public record ListGamesResult(Collection<GameData> games, String message) {
 }
