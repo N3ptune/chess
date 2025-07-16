e7ef6f5c33d9a618a1344dc39c760c5a405398cf
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-15 18:05:27 -0600

    Implement all handlers

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -1,4 +1,27 @@
 package server;
 
-public class ClearHandler {
+
+import model.request.ClearRequest;
+import model.result.ClearResult;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import com.google.gson.Gson;
+
+public class ClearHandler implements Route{
+
+    private final ClearService clearService = new ClearService();
+    private final Gson gson = new Gson();
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+
+        ClearRequest clearRequest = new ClearRequest();
+        ClearResult clearResult = clearService.clear(clearRequest);
+
+        response.status(200);
+
+        return gson.toJson(clearResult);
+    }
 }
