9b953a82c802c88785e3735d37facd2e43f0b3e4
relschool01 <relschool01@gmail.com>
2025-07-28 16:50:09 -0600

    Make main reflect kind of what I want, I think I'll put the switch case in handlePrompt in each of the ui classes

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,33 @@
 import chess.*;
+import facade.ServerFacade;
+import state.ClientState;
+import ui.PostloginUI;
+import ui.PreloginUI;
+
+import java.util.Scanner;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final ClientState state = new ClientState();
+    private static ServerFacade facade;
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        facade = new ServerFacade();
+        System.out.println("♕ Welcome to Chess! Sign in to start. ♕");
+        System.out.println("Options: ");
+        System.out.println("Login as an existing user: \"l\", \"login\" <USERNAME> <PASSWORD>");
+        System.out.println("Register as a new user: \"r\", \"register\" <USERNAME> <PASSWORD> <EMAIL>");
+        System.out.println("Exit the program \"q\", \"quit\"");
+        System.out.println("Print this message: \"h\", \"help\"");
+        System.out.println("Chess >>> ");
+
+        while (true){
+            if (!state.isLoggedIn()){
+                PreloginUI.handleCommand(scanner.nextLine().trim().toLowerCase(), state, facade);
+            } else {
+                PostloginUI.handleCommand(scanner.nextLine().trim().toLowerCase(), state, facade);
+            }
+        }
     }
 }
