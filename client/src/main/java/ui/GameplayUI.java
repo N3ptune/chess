665fde9413bce6e35c758eabf161a03872137c06
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-07-28 23:49:59 -0600

    Save everything before I hop on the plane

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -1,4 +1,17 @@
 package ui;
 
+import facade.ServerFacade;
+import state.ClientState;
+
 public class GameplayUI {
+
+    public static void handleCommand(String command, String[] commandArgs, ClientState state, ServerFacade facade){
+        switch (command){
+            case "help" -> showHelp();
+            default -> System.out.println("Command not recognized. Please try again or type \"help\" for a list of available commands.");
+        }
+    }
+    private static void showHelp(){
+        System.out.println("Nothing to see here yet :)");
+    }
 }
